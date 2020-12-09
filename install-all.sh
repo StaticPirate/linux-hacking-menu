c00405275324dabe3ec5a3e80647048b1c87a99c
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-12-09 15:57:04 -0700

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -18,5 +18,14 @@ apt-get install upgrade
 apt-get install nmap
 apt-get install git
 apt-get install netcat
+sudo apt-get install python
+sudo apt-get install python2
+sudo apt-get install python3
+sudo apt install python3-pip	#pip3
+sudo apt-get install python3-setuptools #setuptools of python3
+sudo apt-get install python-dev #python3 dev tools
+sudo apt-get install libffi-dev
+sudo apt-get install python3-dev
 
 #github installs
+
