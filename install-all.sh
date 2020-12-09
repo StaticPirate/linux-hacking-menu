f24c0da9c0e334fdf262e9981c4d2facf5afa698
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-12-09 15:19:23 -0700

    Create install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Colors
+
+black='\033[0;30m'        # Black
+red='\033[0;31m'          # Red
+green='\033[0;32m'        # Green
+yellow='\033[0;33m'       # Yellow
+blue='\033[0;34m'         # Blue
+purple='\033[0;35m'       # Purple
+cyan='\033[0;36m'         # Cyan
+white='\033[0;37m'        # White
+
+#apt-get installs
+
+apt-get install update
+apt-get install upgrade
+apt-get install nmap
+apt-get install git
+apt-get install netcat
+
+#github installs
